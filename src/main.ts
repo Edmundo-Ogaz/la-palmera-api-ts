f70e3d870cb510539ad02f932049d58494d202a4
Edmundo Ogaz Baltierra <ext_eogazb@falabella.cl>
2023-01-07 14:52:23 -0300

    vercel

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors();
+  app.enableCors({
+    origin: ['http://localhost:3000', 'https://la-palmera-react.vercel.app/'],
+  });
 
   await app.listen(process.env.PORT || 8081);
 }
